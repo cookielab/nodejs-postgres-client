1e447f77360d67044cca3bb845df9d1cb43bbab6
Martin Štekl <martin.stekl@gmail.com>
2018-04-30 14:52:36 +0200

    implement database insert stream

---
+++ src/streams/DatabaseInsertStream.js
@@ -0,0 +1,64 @@
+// @flow
+
+import BatchInsertCollector from '../BatchInsertCollector';
+import {Writable} from 'stream';
+import type {Row} from '../Row';
+
+export default class DatabaseInsertStream extends Writable {
+    batchInsertCollector: BatchInsertCollector;
+    itemsCount: number;
+    promises: Set<Promise<void>>;
+
+    constructor(batchInsertCollector: BatchInsertCollector): void {
+        super({
+            objectMode: true,
+            highWaterMark: batchInsertCollector.batchSize,
+        });
+        this.batchInsertCollector = batchInsertCollector;
+        this.itemsCount = 0;
+        this.promises = new Set();
+    }
+
+    // $FlowFixMe
+    write(row: Row, encoding?: string | (?Error) => void, callback?: (?Error) => void): boolean {
+        return super.write(row, encoding, callback);
+    }
+
+    // $FlowFixMe
+    end(row?: Row | (?Error) => void, encoding?: string | (?Error) => void, callback?: (?Error) => void): void {
+        return super.end(row, encoding, callback);
+    }
+
+    getPromisesForAwait(): Promise<void>[] {
+        const promises = Array.from(this.promises.values());
+        this.itemsCount = 0;
+        this.promises = new Set();
+        return promises;
+    }
+
+    // $FlowFixMe
+    async _write(row: Row, encoding: string, callback: (?Error) => void): Promise<void> {
+        try {
+            this.promises.add(this.batchInsertCollector.add(row));
+            this.itemsCount++;
+
+            // $FlowFixMe
+            if (this.itemsCount >= this.writableHighWaterMark) {
+                await Promise.all(this.getPromisesForAwait());
+            }
+            callback();
+        } catch (error) {
+            callback(error);
+        }
+    }
+
+    async _final(callback: (?Error) => void): Promise<void> {
+        try {
+            this.promises.add(this.batchInsertCollector.flush());
+            await Promise.all(this.getPromisesForAwait());
+            callback();
+        } catch (error) {
+            callback(error);
+        }
+    }
+}
---
+++ tests/streams/DatabaseInsertStream.test.js
@@ -0,0 +1,93 @@
+import BatchInsertCollector from '../../src/BatchInsertCollector';
+import Client from '../../src/Client';
+import columnNamesTransformer from '../../src/transformers/columnNamesTransformer';
+import {createPool} from '../bootstrap';
+import DatabaseInsertStream from '../../src/streams/DatabaseInsertStream';
+import multiInsertTransformer from '../../src/transformers/multiInsertTransformer';
+import {SQL} from 'pg-async';
+import valueListTransformer from '../../src/transformers/valueListTransformer';
+import {WritableStreamAsyncWriter} from '@cookielab.io/stream-async-wrappers';
+
+const TABLE_NAME = 'test_database_insert_stream';
+
+SQL.registerTransform('columnNames', columnNamesTransformer);
+SQL.registerTransform('values', valueListTransformer);
+SQL.registerTransform('multiInsert', multiInsertTransformer);
+
+describe('DatabaseInsertStream', () => {
+    const batchSize = 2;
+
+    let client = null;
+    beforeAll(() => {
+        client = new Client(createPool());
+    });
+    afterAll(async () => {
+        await client.end();
+    });
+
+    let spyOnQuery = null;
+    beforeEach(async () => {
+        await client.query(SQL`CREATE TABLE IF NOT EXISTS $id${TABLE_NAME} (id BIGINT PRIMARY KEY, name TEXT NOT NULL)`);
+        await client.query(SQL`TRUNCATE $id${TABLE_NAME}`);
+        spyOnQuery = jest.spyOn(client, 'query');
+    });
+    afterEach(async () => {
+        spyOnQuery.mockReset();
+        spyOnQuery.mockRestore();
+        spyOnQuery = null;
+        await client.query(SQL`DROP TABLE IF EXISTS $id${TABLE_NAME}`);
+    });
+
+    it('flushes rows on batch size write and on the end', async () => {
+        const batchInsertCollector = new BatchInsertCollector(client, TABLE_NAME);
+        batchInsertCollector.setBatchSize(batchSize);
+        const insertStream = new DatabaseInsertStream(batchInsertCollector);
+
+        const stream = new WritableStreamAsyncWriter(insertStream);
+        for (let i = 0; i < 7; i++) {
+            await stream.write({id: i, name: 'Lorem Ipsum'});
+        }
+        expect(spyOnQuery).toHaveBeenCalledTimes(3);
+        await stream.end();
+        expect(spyOnQuery).toHaveBeenCalledTimes(4);
+    });
+
+    it('triggers error on wrong insert on batch flush', async () => {
+        const batchInsertCollector = new BatchInsertCollector(client, TABLE_NAME);
+        batchInsertCollector.setBatchSize(batchSize);
+        const insertStream = new DatabaseInsertStream(batchInsertCollector);
+        const promise = new Promise((resolve, reject) => {
+            insertStream.once('error', reject);
+            insertStream.once('finish', () => {
+                insertStream.removeListener('error', reject);
+                resolve();
+            });
+        });
+
+        insertStream.write({id: 1, name: 'Lorem Ipsum'});
+        insertStream.write({id: 1, name: 'Lorem Ipsum'});
+        insertStream.end();
+
+        await expect(promise).rejects.toThrow();
+    });
+
+    it('triggers error on wrong insert at the end', async () => {
+        const batchInsertCollector = new BatchInsertCollector(client, TABLE_NAME);
+        batchInsertCollector.setBatchSize(batchSize);
+        const insertStream = new DatabaseInsertStream(batchInsertCollector);
+        const promise = new Promise((resolve, reject) => {
+            insertStream.once('error', reject);
+            insertStream.once('finish', () => {
+                insertStream.removeListener('error', reject);
+                resolve();
+            });
+        });
+
+        insertStream.write({id: 1, name: 'Lorem Ipsum'});
+        insertStream.write({id: 2, name: 'Lorem Ipsum'});
+        insertStream.write({id: 1, name: 'Lorem Ipsum'});
+        insertStream.end();
+
+        await expect(promise).rejects.toThrow();
+    });
+});
