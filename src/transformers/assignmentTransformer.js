78428faf2517a2618a1bb2c34b41ad010459b164
Martin Štekl <martin.stekl@gmail.com>
2018-08-31 13:23:57 +0200

    refactor simple list transformers to use common logic

--- src/transformers/assignmentTransformer.js
+++ src/transformers/assignmentTransformer.js
@@ -2,18 +2,15 @@
 
 import SQL, {SqlFragment} from 'pg-async/lib/sql';
 import {snakeCase} from 'lodash';
+import sqlFragmentMapper from './sqlFragmentMapper';
 import type {Row} from '../Row';
 
 const assignmentTransformer = (row: Row): SqlFragment => {
-    const values = [];
-    for (const key of Object.keys(row)) {
-        values.push(SQL`$identifier${snakeCase(key)} = ${row[key]}`);
-    }
-
-    const glue = values.map((): string => ',\n');
-    glue[0] = ''; // there is no comma in front of the first value
-
-    return new SqlFragment(glue, values);
+    return sqlFragmentMapper(
+        Object.keys(row),
+        (key: string) => SQL`$identifier${snakeCase(key)} = ${row[key]}`,
+        ',\n'
+    );
 };
 
 export default assignmentTransformer;
