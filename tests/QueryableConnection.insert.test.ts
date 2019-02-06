c4ed8a1d6fa584a230580e3b06fbdb3a8df549fb
Martin Štekl <martin.stekl@gmail.com>
2019-02-06 16:11:24 +0100

    fulfill insert test to have complete objects

--- tests/QueryableConnection.insert.test.ts
+++ tests/QueryableConnection.insert.test.ts
@@ -3,16 +3,21 @@ import Client from '../src/Client';
 describe('insert', () => {
     it('converts table name and the values', async () => {
         const connection = new Client(jest.fn() as any);
-        const queryMock = jest.spyOn(connection, 'query').mockImplementation();
+        const queryMock = jest.spyOn(connection, 'query').mockImplementation(() => Promise.resolve({
+            command: 'INSERT',
+            rowCount: 0,
+            oid: 0,
+            fields: [],
+            rows: [],
+        }));
 
         await connection.insert('table', {some: 'value'});
 
         expect(queryMock).toHaveBeenCalledTimes(1);
 
-        const mockCalls = queryMock.mock.calls;
-        const sql = mockCalls[0][0];
-
-        expect(sql.values).toMatchObject(['value']);
-        expect(sql.text.trim()).toMatch('INSERT INTO "table" ("some") VALUES ($1)');
+        expect(queryMock).toHaveBeenNthCalledWith(1, expect.objectContaining({
+            text: expect.stringContaining('INSERT INTO "table" ("some") VALUES ($1)'),
+            values: ['value'],
+        }));
     });
 });
