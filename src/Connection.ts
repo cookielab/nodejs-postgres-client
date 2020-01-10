de203f8ac46a74a9d3f44ff7c4685776d5ddbecb
Lukas Cerny <lukas.cerny@cookielab.io>
2020-01-10 13:12:00 +0100

    fix missing parameter property in getColumn interface

--- src/Connection.ts
+++ src/Connection.ts
@@ -15,7 +15,7 @@ export interface AsyncConnection extends AsyncQueryable {
 
 	getRow(input: QueryConfig | string, values?: any[]): Promise<Row>; // eslint-disable-line @typescript-eslint/no-explicit-any
 	getRows(input: QueryConfig | string, values?: any[]): Promise<Row[]>; // eslint-disable-line @typescript-eslint/no-explicit-any
-	getColumn<T>(input: QueryConfig | string, values?: readonly any[]): Promise<readonly T[]>; // eslint-disable-line @typescript-eslint/no-explicit-any
+	getColumn<T>(input: QueryConfig | string, values?: readonly any[], columnIndex?: number): Promise<readonly T[]>; // eslint-disable-line @typescript-eslint/no-explicit-any
 	insertStream(tableName: string, querySuffix?: string, batchSize?: number): DatabaseInsertStream;
 }
 
