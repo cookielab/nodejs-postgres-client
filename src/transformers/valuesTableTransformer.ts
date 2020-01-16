df3bb73133ff656233fe4574547f09c08b7fb182
Martin Štekl <martin.stekl@gmail.com>
2020-01-16 16:43:26 +0100

    fix (pratially) dependency of expected input for values table transformer

--- src/transformers/valuesTableTransformer.ts
+++ src/transformers/valuesTableTransformer.ts
@@ -3,9 +3,15 @@ import SQL, {SqlFragment} from 'pg-async/lib/sql';
 import sqlFragmentMapper from './sqlFragmentMapper';
 
 const valuesTableTransformer = (rows: Row[]): SqlFragment => {
+	const keys: readonly string[] = rows.length > 0 ? Object.keys(rows[0]) : [];
+
 	return sqlFragmentMapper(
 		rows,
-		(row: Row) => SQL`($values${Object.values(row)})`,
+		(row: Row): SqlFragment => {
+			const data = keys.map((key: string) => row[key] ?? null);
+
+			return SQL`($values${data})`;
+		},
 		',\n'
 	);
 };
--- tests/transformers/valuesTableTransformer.test.ts
+++ tests/transformers/valuesTableTransformer.test.ts
@@ -5,7 +5,7 @@ import valuesTableTransformer from '../../src/transformers/valuesTableTransforme
 SQL.registerTransform('values', valueListTransformer);
 
 describe('values table transformer', () => {
-	it('prepares values table for multiple rows', () => {
+	it('prepares values table for multiple rows for objects with same ordering of keys/columns', () => {
 		const sql = valuesTableTransformer([
 			{id: 'id1', name: 'name1', integer: 1},
 			{id: 'id2', name: 'name2', integer: 2},
@@ -19,6 +19,59 @@ describe('values table transformer', () => {
 			'id2',
 			'name2',
 			2,
+		]);
+	});
+
+	it('prepares values table for multiple rows for objects with different ordering of keys/columns', () => {
+		const sql = valuesTableTransformer([
+			{id: 'id1', name: 'name1', integer: 1},
+			{integer: 2, id: 'id2', name: 'name2'},
+		]);
+
+		expect(sql.text.trim()).toBe('($1, $2, $3),\n($4, $5, $6)');
+		expect(sql.values).toEqual([
+			'id1',
+			'name1',
+			1,
+			'id2',
+			'name2',
+			2,
+		]);
+	});
+
+	it('prepares values table for multiple rows for objects with extra keys/columns in following objects', () => {
+		const sql = valuesTableTransformer([
+			{id: 'id1', name: 'name1', integer: 1},
+			{id: 'id2', name: 'name2', integer: 2, extra: 'foo'},
+		]);
+
+		expect(sql.text.trim()).toBe('($1, $2, $3),\n($4, $5, $6)');
+		expect(sql.values).toEqual([
+			'id1',
+			'name1',
+			1,
+			'id2',
+			'name2',
+			2,
+		]);
+	});
+
+	it('prepares values table for multiple rows for objects with extra keys/columns in the first object (with missing keys/columns in following objects)', () => {
+		const sql = valuesTableTransformer([
+			{id: 'id1', name: 'name1', integer: 1, extra: 'bar'},
+			{id: 'id2', name: 'name2', integer: 2},
+		]);
+
+		expect(sql.text.trim()).toBe('($1, $2, $3, $4),\n($5, $6, $7, $8)');
+		expect(sql.values).toEqual([
+			'id1',
+			'name1',
+			1,
+			'bar',
+			'id2',
+			'name2',
+			2,
+			null,
 		]);
 	});
 
