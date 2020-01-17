40347957a6441f1f82fb684f06b7f6cc88b8c7c4
Martin Štekl <martin.stekl@gmail.com>
2020-01-17 11:40:12 +0100

    add typeName property ty type not found error

--- src/errors/TypeNotFoundError.ts
+++ src/errors/TypeNotFoundError.ts
@@ -1,6 +1,9 @@
 class TypeNotFoundError extends Error {
+	public readonly typeName: string;
+
 	public constructor(typeName: string) {
 		super(`Type "${typeName}" not found.`);
+		this.typeName = typeName;
 	}
 }
 
