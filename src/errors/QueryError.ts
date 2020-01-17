78696d9630a206465826f7048a11712d5456d529
Martin Štekl <martin.stekl@gmail.com>
2020-01-17 11:39:34 +0100

    allow setting query error reason once only

--- src/errors/QueryError.ts
+++ src/errors/QueryError.ts
@@ -3,12 +3,23 @@ import {QueryConfig} from 'pg';
 class QueryError extends Error {
 	public readonly query: string;
 	public readonly values: readonly any[] | undefined; // eslint-disable-line @typescript-eslint/no-explicit-any
-	public causedBy: Error | undefined;
+	private _causedBy: Error | undefined;
 
 	public constructor(input: QueryConfig | string, values?: readonly any[]) { // eslint-disable-line @typescript-eslint/no-explicit-any
 		super('Query execution failed.');
 		this.query = typeof input === 'string' ? input : input.text;
 		this.values = typeof input === 'string' ? values : input.values;
+	}
+
+	public get causedBy(): Error | undefined {
+		return this._causedBy;
+	}
+
+	public set causedBy(value: Error | undefined) {
+		if (this._causedBy != null) {
+			throw new Error('Error reason (caused by) was already set.');
+		}
+		this._causedBy = value;
 	}
 }
 
