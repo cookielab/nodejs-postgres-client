1d0142b1dc83f67a7108dd13a1e55c4d69a4e49e
Martin Štekl <martin.stekl@gmail.com>
2020-01-23 15:53:37 +0100

    optimize sql fragment mapper a bit

--- src/transformers/sqlFragmentMapper.ts
+++ src/transformers/sqlFragmentMapper.ts
@@ -1,12 +1,17 @@
 import {SqlFragment} from 'pg-async/lib/sql';
 
 const sqlFragmentMapper = <T>(input: readonly T[], valueMapper: (value: T) => SqlFragment, glueString: string): SqlFragment => {
+	if (input.length < 1) {
+		return new SqlFragment([], []);
+	}
+
 	const values: readonly SqlFragment[] = input.map((value: T) => valueMapper(value));
 
-	const glue = values.map(() => glueString);
-	glue[0] = ''; // there is no glue in front of the first value
+	const glues = new Array(values.length)
+		.fill('', 0, 1) // there is empty string glue in front of the first value
+		.fill(glueString, 1);
 
-	return new SqlFragment(glue, values);
+	return new SqlFragment(glues, values);
 };
 
 export default sqlFragmentMapper;
