0df81dac5fc0857fe32d5f4477196070288cf36d
Martin Štekl <martin.stekl@gmail.com>
2020-01-23 16:34:06 +0100

    add test for event emitted by read stream

--- tests/streams/DatabaseReadStream.test.ts
+++ tests/streams/DatabaseReadStream.test.ts
@@ -6,4 +6,19 @@ describe('DatabaseReadStream', () => {
 	it('inherits pg-query-stream', () => {
 		expect(new DatabaseReadStream('SELECT 42 AS theAnswer')).toBeInstanceOf(QueryStream);
 	});
+
+	it('emits "close" event on destroy', async () => {
+		const stream = new DatabaseReadStream('SELECT 42 AS theAnswer');
+
+		const promise = new Promise((resolve: () => void) => {
+			stream.once('close', () => {
+				resolve();
+			});
+		});
+
+		stream.destroy();
+
+		// The promise will not be resolved if the event is not emitted and the test will fail on timeout.
+		await promise;
+	});
 });
