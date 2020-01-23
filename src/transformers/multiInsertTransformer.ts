f73aec4fce06b234f89e861329f0eee4166701ff
Martin Štekl <martin.stekl@gmail.com>
2020-01-23 15:52:31 +0100

    use more strict condition for minimum rows

--- src/transformers/multiInsertTransformer.ts
+++ src/transformers/multiInsertTransformer.ts
@@ -2,7 +2,7 @@ import {Row} from '../Row';
 import SQL, {SqlFragment} from 'pg-async/lib/sql';
 
 const multiInsertTransformer = <T extends Row>(rows: readonly T[]): SqlFragment => {
-	if (rows.length <= 0) {
+	if (rows.length < 1) {
 		throw new Error('Cannot format multi insert for no rows.');
 	}
 	const columnNames: ReadonlyArray<keyof T> = Object.keys(rows[0]);
